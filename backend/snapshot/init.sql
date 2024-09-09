504dae5fd055aab58ef2d3b42f3df9789c0ff394
PandoraArc <parinthon.nea@gmail.com>
2024-09-10 00:07:52 +0700

    add new colunm to db

--- backend/snapshot/init.sql
+++ backend/snapshot/init.sql
@@ -2,6 +2,8 @@ CREATE TABLE session (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     segmentation_path VARCHAR(255) DEFAULT NULL,
     classification_path VARCHAR(255) DEFAULT NULL,
+    classification_data VARCHAR(255) DEFAULT NULL,
     transformation_path VARCHAR(255) DEFAULT NULL,
+    transformation_data VARCHAR(255) DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
